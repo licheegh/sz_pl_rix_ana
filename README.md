f2e77d85c02bbbe609271248b4a43bb6634f9016
lichee <luleier@gmail.com>
2012-07-03 23:02:36 +0800

    add a text input to input room number

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ sz_pl_rix_ana
 =============
 
 analyzer for http://ris.szpl.gov.cn/bol/
+
+todo:
+	1.add auto word wrap to GUI
--- gui.py
+++ gui.py
@@ -11,28 +11,41 @@ class Application(Frame):
 		Frame.__init__(self,master)
 		self.grid()
 		self.value_entered=StringVar()
+		self.room_number=StringVar()
 		self.result_display=StringVar()
+		self.stat={}
 		self.creatWidgets()
 
 	def get_stat(self):
 		target_url=self.value_entered.get()
+		room=self.room_number.get()
 		print 'entered url:'+target_url
-		stat=analyzer.szpl_ana(target_url)
+		self.stat=analyzer.szpl_ana(target_url)
+		stat=self.stat
 		print stat
-		string_to_display='result:\n'
-		for item in stat.keys():
-			string_to_display+=item+':'+str(stat[item])+'\n'
-		string_to_display+='\n'
-		string_to_display+='{:.2%}'.format(stat['sold']['total']*1.0/stat['total'])+' sold'+'\n'
-		string_to_display+='{:.2%}'.format((stat['sold']['total']+stat['signed']['total']+stat['pre_signed']['total'])*1.0/stat['total'])+'% signed&sold'+'\n'
+		string_to_display='result:\n' 
+		#for item in stat.keys():
+		#	string_to_display+=item+':'+str(stat[item])+'\n'
+		#string_to_display+='\n'
+		#string_to_display+='{:.2%}'.format(stat['sold']['total']*1.0/stat['total'])+' sold'+'\n'
+		#string_to_display+='{:.2%}'.format((stat['sold']['total']+stat['signed']['total']+stat['pre_signed']['total'])*1.0/stat['total'])+'% signed&sold'+'\n'
+		string_to_display+='Total Room:'+str(stat['total'])+' Total Sale:'+str(stat['pre_signed']+stat['sold']+stat['signed'])
+		if room!='':
+			string_to_display+='\nRoom'+room+':'+str(stat[room])
 		self.result_display.set(string_to_display)
-
 		backup_file=open('./saved_data.txt','w')
 		backup_file.write(time.asctime(time.localtime())+'\n')
 		backup_file.write('target_url:'+target_url+'\n')
 		backup_file.write(string_to_display)
 		backup_file.close()
 
+
+	def get_room(self):
+		room=self.room_number.get()
+		#print self.stat
+		if room!='':
+			string_to_display='\nRoom'+room+':'+str(self.stat[room])
+			self.result_display.set(string_to_display)
 
 	def creatWidgets(self):
 		self.quitButton=Button(self,text='quit',command=self.quit)
@@ -46,6 +59,12 @@ class Application(Frame):
 
 		self.outputLabel=Label(self,height=10,width=70,justify=LEFT,textvariable=self.result_display)
 		self.outputLabel.grid(column=1,row=2)
+
+		self.valueEntry=Entry(self,width=70,textvariable=self.room_number)
+		self.valueEntry.grid(column=1,row=3)
+		
+		self.funButton=Button(self,text='get room info',command=self.get_room)
+		self.funButton.grid(column=2,row=3)
 
 
 print '\n---+++   web page analyzer   +++---\n'
